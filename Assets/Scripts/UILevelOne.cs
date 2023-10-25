22a21cb91bf664eed36bcfe8e9bab3f94e592f73
kimlengthai <141606539+kimlengthai@users.noreply.github.com>
2023-10-25 14:05:08 +1100

    Create a script for Lvl1 button.
    Just make a script for level 1 button to load into another scene.

---
+++ Assets/Scripts/UILevelOne.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILevelOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+/*    public void OnStartButtonPressed()
+    {
+        Invoke(nameof(LoadFirstLevel), 1.0f);
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadSceneAsync(1);
+        DontDestroyOnLoad(gameObject);
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
