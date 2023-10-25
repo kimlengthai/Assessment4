4befbc5b23878097f8e219e4a7ba8dc20df4680d
kimlengthai <141606539+kimlengthai@users.noreply.github.com>
2023-10-25 15:12:01 +1100

    Fixing the animation and level 1 button

--- Assets/Scripts/UILevelOne.cs
+++ Assets/Scripts/UILevelOne.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UILevelOne : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+       // SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-/*    public void OnStartButtonPressed()
+    /*public void OnStartButtonPressed()
     {
         Invoke(nameof(LoadFirstLevel), 1.0f);
+    }*/
+
+    public void LoadFirstLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+      //  SceneManager.LoadSceneAsync(1);
+        DontDestroyOnLoad(gameObject);
     }
 
-    public void LoadFirstLevel()
+    /*public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        SceneManager.LoadSceneAsync(1);
-        DontDestroyOnLoad(gameObject);
+        if (scene.buildIndex == 1)
+        {
+            
+        }
     }*/
 
     // Update is called once per frame
