bcddae01bd29066a3428f5e0551e51cc42cba579
kimlengthai <141606539+kimlengthai@users.noreply.github.com>
2023-10-27 05:51:20 +1100

    Coding inside my PacStudentMovement

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // public float moveSpeed = 3.0f;
+    // private Vector3 targetPos;
+    // private Vector3 nextPos;
+    // private Vector3 currentInput = Vector3.zero;
+    // private Vector3 lastInput = Vector3.zero;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     targetPos = new Vector3(0.5f, -0.5f, 0f);
+    //     transform.position = targetPos;
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (!isLerping())
+    //     {
+    //         getPacStudentInput();
+    //     }
+    // }
+
+    // bool isLerping()
+    // {
+    //     return transform.position != targetPos;
+    // }
+
+    // public void getPacStudentInput()
+    // {
+    //     Vector3 inputDirection = Vector3.zero;
+
+    //     if (Input.GetKey(KeyCode.W))
+    //     {
+    //         inputDirection = Vector3.up;
+    //     }
+    //     else if (Input.GetKey(KeyCode.A))
+    //     {
+    //         inputDirection = Vector3.left;
+    //     }
+    //     else if (Input.GetKey(KeyCode.S))
+    //     {
+    //         inputDirection = Vector3.down;
+    //     }
+    //     else if (Input.GetKey(GetCode.D))
+    //     {
+    //         inputDirection = Vector3.right;
+    //     }
+
+    //     if (inputDirection != Vector3.zero)
+    //     {
+    //         lastInput = inputDirection;
+    //     }
+    // }
+
 }
