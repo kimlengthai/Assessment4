bd9204b7dc07bd22051b6c038e4b8398441a39af
kimlengthai <141606539+kimlengthai@users.noreply.github.com>
2023-10-27 12:11:02 +1100

    Test if my PacStudent works or not
    Unfortunately, it does not work what the specification says but i'll try my best to work it out.

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -1,5 +1,7 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
@@ -57,4 +59,73 @@ public class PacStudentController : MonoBehaviour
     //     }
     // }
 
+}
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+//using System.Collections.Specialized;
+//using System.Diagnostics;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    int switchingHead;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Animator animatorController;
+    private Tweener tweener;
+    Vector3 topLeft = new Vector3(0.5f, -0.5f, 0f);
+    Vector3 topRight = new Vector3(3.5f, -0.5f, 0f);
+    Vector3 bottomRight = new Vector3(3.5f, -2.5f, 0f);
+    Vector3 bottomLeft = new Vector3(0.5f, -2.5f, 0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int speed = 1;
+        if (Input.GetKeyDown("w"))
+        {
+            animatorController.SetTrigger("isFlipped");
+            float fractionTime = Vector3.Distance(topLeft, topRight) / speed;
+            tweener.AddTween(player.transform, player.transform.position, player.transform.position + Vector3.up, fractionTime);
+            switchingHead = 3;
+            animatorController.SetInteger("switchingHead", switchingHead);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            animatorController.SetTrigger("isFlipped");
+            float fractionTime = Vector3.Distance(topLeft, topRight) / speed;
+            tweener.AddTween(player.transform, player.transform.position, bottomRight, fractionTime);
+            switchingHead = 2;
+            animatorController.SetInteger("switchingHead", switchingHead);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            animatorController.SetTrigger("isFlipped");
+            float fractionTime = Vector3.Distance(topLeft, topRight) / speed;
+            tweener.AddTween(player.transform, player.transform.position, bottomLeft, fractionTime);
+            switchingHead = 1;
+            animatorController.SetInteger("switchingHead", switchingHead);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            animatorController.SetTrigger("isFlipped");
+            float fractionTime = Vector3.Distance(topLeft, topRight) / speed;
+            tweener.AddTween(player.transform, player.transform.position, topLeft, fractionTime);
+            switchingHead = 0;
+            animatorController.SetInteger("switchingHead", switchingHead);
+        }
+
+    }
 }
